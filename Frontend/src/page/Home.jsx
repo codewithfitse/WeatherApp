a0ff518b4f0ef49ef88df5b7ebfd74112a2b6b88
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-29 19:19:04 +0300

    WORKING ON HOMEPAGE UI

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
     <>
       <div className="w-full h-screen text-white bg-gray-900 flex justify-center">
         <div className="w-[80%] h-[70%] mt-[10px] p-3 flex flex-col items-center justify-center border-2 border-amber-50 bg-white/5 backdrop-blur-2xl rounded-2xl space-y-3">
-          <p className="">Italiy</p>
+          <p className="">Italiy, it</p>
           <img
             src="logo.webp"
             alt="WeatherPhoto"
@@ -20,6 +20,16 @@ export const Home = () => {
             <p className="">11km</p>
             <p className="">02%</p>
             <p className="">8hr</p>
+          </div>
+          <div className="w-full flex justify-center space-x-6">
+            <input
+              type="text"
+              className="w-[60%] p-3 bg-amber-50 text-black rounded-2xl"
+              placeholder="Enter City..."
+            />
+            <button className="p-3 rounded-2xl bg-gray-800 text-accent">
+              Submit
+            </button>
           </div>
         </div>
       </div>
