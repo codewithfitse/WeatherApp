90b64208ee18a1d3e62294afdc6bd7e36868e663
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-29 19:15:51 +0300

    WORKING ON HOMEPAGE UI

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -5,14 +5,18 @@ export const Home = () => {
 
   return (
     <>
-      <div className="w-full h-screen bg-gray-900 flex justify-center items-center">
-        <div className="w-[80%] h-[70%] p-3 flex flex-col items-center justify-center border-2 border-amber-50 bg-white/5 backdrop-blur-2xl rounded-2xl">
+      <div className="w-full h-screen text-white bg-gray-900 flex justify-center">
+        <div className="w-[80%] h-[70%] mt-[10px] p-3 flex flex-col items-center justify-center border-2 border-amber-50 bg-white/5 backdrop-blur-2xl rounded-2xl space-y-3">
           <p className="">Italiy</p>
-          <h1 className="">Italiy</h1>
-          <img src="size-24" alt="WeatherPhoto" srcSet="" />
+          <img
+            src="logo.webp"
+            alt="WeatherPhoto"
+            className="size-24"
+            srcSet=""
+          />
           <h1 className="">29c</h1>
           <p className="">MSG</p>
-          <div className="flex justify-between">
+          <div className="w-full flex justify-around">
             <p className="">11km</p>
             <p className="">02%</p>
             <p className="">8hr</p>
