b1a31a76fc11f895891e14b3f5cf668cfb77c7d8
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-29 20:13:37 +0300

    WORKING ON HOMEPAGE UI

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -24,7 +24,7 @@ export const Home = () => {
       <div className="w-full h-screen text-white bg-gray-900 flex justify-center">
         <div className="w-[80%] h-[70%] mt-[10px] p-3 flex flex-col items-center justify-center border-2 border-amber-50 bg-white/5 backdrop-blur-2xl rounded-2xl space-y-3">
           <p className="">
-            {weather?.name}, {weather?.country}
+            {weather?.name}, {weather?.sys?.country}
           </p>
           <img
             src="logo.webp"
@@ -33,11 +33,20 @@ export const Home = () => {
             srcSet=""
           />
           <h1 className="">{weather?.main?.temp}°F</h1>
-          <p className=""></p>
+          <p className="">{weather?.weather?.[0].main}</p>
           <div className="w-full flex justify-around">
-            <p className="">{weather?.wind?.speed}km</p>
-            <p className="">{weather?.main?.humidity}%</p>
-            <p className="">8hr</p>
+            <div className="flex space-x-2">
+              <img src="wind.svg" className="!text-blue-400" alt="" />
+              <p className="">{weather?.wind?.speed}km</p>
+            </div>
+            <div className="flex space-x-2">
+              <img src="wind.svg" className="!text-blue-400" alt="" />
+              <p className="">{weather?.main?.humidity}%</p>
+            </div>
+            <div className="flex space-x-2">
+              <img src="wind.svg" className="!text-blue-400" alt="" />
+              <p className="">{weather?.wind?.speed}km</p>
+            </div>
           </div>
           <div className="w-full flex justify-center space-x-6">
             <form action="" onSubmit={handleSearch}>
