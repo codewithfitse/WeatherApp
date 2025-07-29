96ace4a0ceba76b17e623884889f9ffcfa885dff
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-29 19:00:59 +0300

    WORKING ON HOMEPAGE UI

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
 
 export const Home = () => {
-  //const [input, setInput] = useState(0);
+  const [input, setInput] = useState(0);
 
   return (
     <>
       <div className="w-full h-screen bg-gray-900 flex justify-center items-center">
-        <div className="w-[80%] h-[70%] p-3 flex justify-center border-2 border-amber-50 bg-white/5 backdrop-blur-2xl rounded-2xl">
+        <div className="w-[80%] h-[70%] p-3 flex flex-col items-center justify-center border-2 border-amber-50 bg-white/5 backdrop-blur-2xl rounded-2xl">
           <p className="">Italiy</p>
           <h1 className="">Italiy</h1>
-          <h1 className="">Italiy</h1>
-          <img src="" alt="WeatherPhoto" srcSet="" />
-          <p className="">Italiy</p>
+          <img src="size-24" alt="WeatherPhoto" srcSet="" />
+          <h1 className="">29c</h1>
+          <p className="">MSG</p>
+          <div className="flex justify-between">
+            <p className="">11km</p>
+            <p className="">02%</p>
+            <p className="">8hr</p>
+          </div>
         </div>
       </div>
     </>
