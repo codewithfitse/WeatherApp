d3ae5ba744846cc87b189b8ca686d22a837156bb
Fitsum Zerihun <fitsa0909@gmail.com>
2025-07-29 18:56:26 +0300

    WORKING ON HOMEPAGE UI

--- Frontend/src/page/Home.jsx
+++ Frontend/src/page/Home.jsx
@@ -1,81 +1,18 @@
 import { useState } from "react";
 
 export const Home = () => {
-  const [input, setInput] = useState(0);
+  //const [input, setInput] = useState(0);
 
   return (
     <>
-      <div
-        className={`w-full min-h-screen flex flex-col items-center overflow-x-hidden bg-primary dark:bg-primary-dark text-primary-dark dark:text-primary ${
-          isDark ? "dark" : ""
-        }`}
-      >
-        <header className="w-full h-[70px] px-3 flex justify-between items-center bg-gray-300 dark:bg-gray-500">
-          <h2 className="">Expense Tracker</h2>
-          <button
-            className="w-fit h-fit p-3 text-2xl rounded-2xl bg-primary dark:bg-primary-dark"
-            onClick={toogle}
-          >
-            {isDark ? (
-              <i className="fa fa-sun-o !text-[36px] !text-accent"></i>
-            ) : (
-              <i className="fa fa-moon-o !text-[36px] text-primary-dark"></i>
-            )}
-          </button>
-        </header>
-        <main className="w-full h-full">
-          <div className="card m-3">
-            <p className="">$Total Balance</p>
-            <h1 className="">${state.count}</h1>
-          </div>
-          <div className="flex">
-            <div className="card m-3" onClick={() => dispatch({ type: "inc" })}>
-              <p>$Deposit Balance</p>
-              <h1>+500</h1>
-            </div>
-            <div className="card m-3" onClick={() => dispatch({ type: "dec" })}>
-              <p>$Withdraw Balance</p>
-              <h1>-250</h1>
-            </div>
-          </div>
-
-          <form
-            onSubmit={calculate}
-            className="py-2 px-5 flex items-center space-x-3"
-          >
-            <label className="text-2xl">Amount</label>
-            <input
-              type="number"
-              value={input}
-              className="w-full h-fit p-3 bg-accent dark:bg-gray-700 text-black dark:text-white rounded-2xl"
-              placeholder="Type.."
-              onChange={(e) => setInput(e.target.value)}
-            />
-            <button
-              type="submit"
-              className="p-3 rounded-2xl bg-gray-700 text-white"
-            >
-              Done
-            </button>
-          </form>
-
-          <h1 className="my-3 ml-3 text-xl">Recent Transactions</h1>
-          <div>
-            {transactions.map((item, index) => (
-              <div
-                key={index}
-                className={`m-3 flex justify-between items-center p-4 rounded-xl shadow cursor-pointer transition ${
-                  item.amount < 0
-                    ? "bg-green-100 text-green-800 dark:bg-red-100 dark:text-red-900"
-                    : "bg-green-100 text-green-800 dark:bg-green-100 dark:text-green-900"
-                }`}
-              >
-                <p className="text-lg font-medium">{item.label}</p>
-                <h1 className="text-xl font-bold">${item.amount}</h1>
-              </div>
-            ))}
-          </div>
-        </main>
+      <div className="w-full h-screen bg-gray-900 flex justify-center items-center">
+        <div className="w-[80%] h-[70%] p-3 flex justify-center border-2 border-amber-50 bg-white/5 backdrop-blur-2xl rounded-2xl">
+          <p className="">Italiy</p>
+          <h1 className="">Italiy</h1>
+          <h1 className="">Italiy</h1>
+          <img src="" alt="WeatherPhoto" srcSet="" />
+          <p className="">Italiy</p>
+        </div>
       </div>
     </>
   );
